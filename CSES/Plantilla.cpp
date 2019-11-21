b43c648b71d0a2a2b941ef015650f81c793298cb
julianferres <julianferres@gmail.com>
2019-11-20 21:43:32 -0300

    Update C++ Template

--- CSES/Plantilla.cpp
+++ CSES/Plantilla.cpp
@@ -3,18 +3,19 @@ using namespace std;
 typedef long long ll;
 typedef vector<ll> vi;
 #define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-#define fore(i, a, b) for(ll i = (a); i < (ll) (b); i++)
-#define forn(i, n) fore(i, 0, n)
-#define F first
-#define S second
+#define forr(i, a, b) for(int i = (a); i < (int) (b); i++)
+#define forn(i, n) forr(i, 0, n)
 #define pb push_back
 #define mp make_pair
-#define sz size()
+#define all(c) (c).begin(),(c).end()
 #define DBG(x) cerr << #x << " = " << (x) << endl
+#define DBGV(v,n) forn(i,n) cout << v[i] << " "; cout << endl
+#define esta(x,c) ((c).find(x) != (c).end())
 #define RAYA cerr << "===============================" << endl
 
-int main(){ 
-    FIN;
+int main()
+{
+	FIN;
 
     return 0;
 }
