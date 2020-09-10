7a69beea83be56a81c184514c43df6ea62edc60a
Julian Ferres <julianferres@gmail.com>
2020-09-09 23:55:00 -0300

    C, D Step 1

--- Codeforces EDU/DSU/Step 1/C_Experience.cpp
+++ Codeforces EDU/DSU/Step 1/C_Experience.cpp
@@ -1,6 +1,37 @@
-/*    AUTHOR: julianferres, Wed 09 Sep 2020 08:51:58 PM -03 */
+/*    AUTHOR: julianferres, Wed 09 Sep 2020 11:31:02 PM -03 */
 #include <bits/stdc++.h>
 using namespace std;
+
+// DEBUGGER
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+//END DEBUGGER
 
 typedef long long ll;
 typedef vector<ll> vi;
@@ -17,23 +48,48 @@ const int INF = 1<<30; // const ll INF = 1LL<<60;
 const int MOD = 1e9+7; // 998244353
 const int MAXN  = 2e5+5;
 
-int par[MAXN];
-int find(int x){return par[x]=par[x]==x?x:find(par[x]);}
-void join(int x, int y){par[find(x)]=find(y);}
+vector<int> par, delta;
+
+int find(int x){
+    while(par[x] != x)
+        x = par[x];
+    return x;
+}
+void join(int x, int y){
+    if(find(x) == find(y)) return;
+    par[find(x)] = par[find(y)] = (int) par.size();
+    par.pb((int) par.size());
+    delta.pb(0);
+}
+int sum(int x){
+    int ans = 0;
+    while(x != par[x])
+    {
+        ans += delta[x];
+        x = par[x];
+    }
+    return ans + delta[x];
+}
 
 
 int main(){  
     FIN;
     int n, m; cin >> n >> m;
+    par.resize(n), delta.resize(n, 0);
     forn(i, n) par[i] = i;
     forn(i, m){
         string type; cin >> type;
-        int u, v; cin >> u >> v;
-        u--, v--;
-        if(type == "union"){
+        int u, v; cin >> u; u--;
+        if(type == "add"){
+            cin >> v;
+            delta[find(u)] += v;
+        } 
+        if(type == "join"){
+            cin >> v; v--;
             join(u, v);
-        } else {
-            cout << ((find(u) == find(v)) ? "YES" : "NO") << "\n";
+        }
+        if(type == "get") {
+            cout << sum(u) << "\n";
         }
     }
     return 0;
